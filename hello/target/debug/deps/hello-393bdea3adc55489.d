3696bf46a0610ab10b196cafaf4d93b0f001e640
Eric Hao <hhao99@gmail.com>
2023-05-02 14:11:24 -0500

    remove target

--- hello/target/debug/deps/hello-393bdea3adc55489.d
+++
@@ -1,5 +0,0 @@
-/Users/k3/repos/rust/learn_rust/hello/target/debug/deps/hello-393bdea3adc55489.rmeta: src/main.rs
-
-/Users/k3/repos/rust/learn_rust/hello/target/debug/deps/hello-393bdea3adc55489.d: src/main.rs
-
-src/main.rs:
--- hello/target/debug/deps/hello-6bd673a7f1bc3962.d
+++
@@ -1,5 +0,0 @@
-/Users/k3/repos/rust/learn_rust/hello/target/debug/deps/hello-6bd673a7f1bc3962.rmeta: src/main.rs
-
-/Users/k3/repos/rust/learn_rust/hello/target/debug/deps/hello-6bd673a7f1bc3962.d: src/main.rs
-
-src/main.rs:
