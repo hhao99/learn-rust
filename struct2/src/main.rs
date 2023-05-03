bfc861eb1e1e679a174b9113887b10c398371391
Eric Hao <hhao99@gmail.com>
2023-05-03 11:18:29 -0500

    add struct exercise

--- struct2/src/main.rs
+++ struct2/src/main.rs
@@ -9,13 +9,22 @@ fn main() {
     let n2 = Number { odd: false, num: 31};
 
     print_number(n1);
-    print_number(n2);
+    print_number2(n2);
 }
 
 fn print_number(n: Number) {
-    if( n.odd) {
+    if n.odd {
         println!("Odd Number {}", n.num);
     } else {
+        println!("Even Number {}", n.num);
+    }
+    
+}
+
+fn print_number2(n: Number) {
+    if let Number { odd: true, .. } = n {
+        println!("Odd Number {}", n.num);
+    } else if let Number { odd: false, .. } = n {
         println!("Even Number {}", n.num);
     }
     
